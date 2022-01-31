b6290598757e667c7d801aeafeb1666e78707cd0
Som23Git <somesh.rokz@gmail.com>
2022-02-01 03:35:12 +0530

    Initial Codebase Ver 1.0

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Simple_html_css_js_CALC
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Som23/pen/xxPZzav](https://codepen.io/Som23/pen/xxPZzav).
+
+
---
+++ src/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  let one_button = document.getElementById("myButton_1");
+  let c = document.getElementById("display");
+  c.oninput = function(){
+      for(let i=0;i<3;i++){
+        console.log(i);
+      }; console.log("for_looped_closed");
+    };
+  one_button.onclick = function (){
+    console.log(one_button.value);
+    c = one_button.value;
+    console.log(c+"=Display Value");
+  };
+};
